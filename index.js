5f96faf8e395ff4a984e9cf20838ead23dea931e
Damian <wisdomdamian5@gmail.com>
2026-09-03 05:27:54 +0000

    create javascript example

---
+++ index.js
@@ -0,0 +1,11 @@
+/*console.log("hello");
+let fruit="apple"
+console.log(fruit)
+let[a,b]=[1,3]
+console.log(b,a)*/
+let namee="baby";
+let age=20;
+console.log("hello");
+console.log(namee);
+console.log(age);
+
